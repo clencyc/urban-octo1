e4b5c5d14f8be8a1f8096a086bfac4a88fe5a273
emobilis <clency2023@gmail.com>
2023-02-23 10:11:06 +0300

    added css styling to djando form

--- djangoProject1/views.py
+++ djangoProject1/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import UserReg
+from .forms import MembersReg
+
 
 def reg(request):
     submit_button = request.POST.get("christine")
@@ -12,5 +14,27 @@ def reg(request):
         name = reg_form.cleaned_data.get("name")
         email = reg_form.cleaned_data.get("email")
         password = reg_form.cleaned_data.get("password")
-    context = { 'reg_form'  : reg_form, 'name': name, 'email' :email, 'submit_button':   submit_button }
+    context = {'reg_form': reg_form, 'name': name, 'email': email, 'submit_button': submit_button}
     return render(request, 'register.html', context)
+
+
+def reg_members(request):
+    submit_members = request.POST.get("member")
+    name = ''
+    age = ''
+    phone = ''
+    city = ''
+    country = ''
+
+    reg_members_form = MembersReg(request.POST or None)
+    if reg_members_form.is_valid():
+        name = reg_members_form.cleaned_data.get("name")
+        age = reg_members_form.cleaned_data.get("age")
+        phone = reg_members_form.cleaned_data.get("phone")
+        city = reg_members_form.cleaned_data.get("city")
+        country = reg_members_form.cleaned_data.get("country")
+
+    context = {'reg_members_form': reg_members_form, 'name': name, 'age': age, 'phone': phone, 'city': city,
+               'country': country
+               }
+    return render(request, 'members.html', context)
